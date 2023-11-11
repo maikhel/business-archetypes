2cb3900017fd3b03aad74c6da2e39efc021d9a48
maikhel <mlecicki@gmail.com>
2023-11-11 13:50:27 +0100

    Add person model

---
+++ db/migrate/20231110185942_create_people.rb
@@ -0,0 +1,12 @@
+class CreatePeople < ActiveRecord::Migration[7.1]
+  def change
+    create_table :people, id: :uuid do |t|
+      t.string :name
+      t.string :first_name
+      t.string :last_name
+      t.date :date_of_birth
+
+      t.timestamps
+    end
+  end
+end
