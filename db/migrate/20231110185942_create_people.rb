8c6675ccea1658559460bf46d45625d97a599349
maikhel <mlecicki@gmail.com>
2023-11-15 18:01:47 +0100

    Update person model

--- db/migrate/20231110185942_create_people.rb
+++ db/migrate/20231110185942_create_people.rb
@@ -2,7 +2,7 @@
 
 class CreatePeople < ActiveRecord::Migration[7.1]
   def change
-    create_table :people, id: :uuid do |t|
+    create_table :people do |t|
       t.string :name
       t.string :first_name
       t.string :last_name
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,22 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_231_111_131_819) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_10_185942) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'organizations', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
+    t.date "date_of_birth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'people', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.string 'name'
-    t.string 'first_name'
-    t.string 'last_name'
-    t.date 'date_of_birth'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
 end
