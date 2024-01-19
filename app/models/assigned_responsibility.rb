9d8829abd48bad33fd85614e94e52661380a3503
maikhel <mlecicki@gmail.com>
2024-01-19 13:18:08 +0100

    Add assigned responsibility model

---
+++ app/models/assigned_responsibility.rb
@@ -0,0 +1,2 @@
+class AssignedResponsibility < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_19_121602) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_19_121723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assigned_responsibilities", force: :cascade do |t|
+    t.bigint "responsibility_id"
+    t.bigint "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["person_id"], name: "index_assigned_responsibilities_on_person_id"
+    t.index ["responsibility_id"], name: "index_assigned_responsibilities_on_responsibility_id"
+  end
 
   create_table "party_relationships", force: :cascade do |t|
     t.string "name"
